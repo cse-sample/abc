a955eb34b46231d351e6457551a6fae24b042ff7
paas <paas@cse-codegen-backend-3882710532-pvvsc>
2018-08-16 02:05:32 +0000

    Auto pushed by ServiceStage!

---
+++ src/main/java/com/service/testsource/controller/TestsourceDelegate.java
@@ -0,0 +1,14 @@
+package com.service.testsource.controller;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class TestsourceDelegate {
+
+    public String helloworld(String name){
+
+        // Do Some Magic Here!
+        return name;
+    }
+}
